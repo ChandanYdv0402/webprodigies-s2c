72dc7551fe93a1893d7324a73bdfb48be1eabc47
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-10-07 03:11:33 +0530

    changed the signup block

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -8,19 +8,100 @@ export default function LoginPage() {
         <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
             <form
                 action=""
-                className="bg-card m-auto h-fit w-full max-w-sm rounded-[calc(var(--radius)+.125rem)] border p-0.5 shadow-md dark:[--color-muted:var(--color-zinc-900)]">
-                <div className="p-8 pb-6">
-                    <div>
+                className="bg-muted m-auto h-fit w-full max-w-sm overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]">
+                <div className="bg-card -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
+                    <div className="text-center">
                         <Link
                             href="/"
-                            aria-label="go home">
+                            aria-label="go home"
+                            className="mx-auto block w-fit">
                             
                         </Link>
-                        <h1 className="mb-1 mt-4 text-xl font-semibold">Create a Tailark Account</h1>
+                        <h1 className="mb-1 mt-4 text-xl font-semibold">Create a S2C Account</h1>
                         <p className="text-sm">Welcome! Create an account to get started</p>
                     </div>
 
-                    <div className="mt-6 grid grid-cols-2 gap-3">
+                    <div className="mt-6 space-y-6">
+                        <div className="grid grid-cols-2 gap-3">
+                            <div className="space-y-2">
+                                <Label
+                                    htmlFor="firstname"
+                                    className="block text-sm">
+                                    Firstname
+                                </Label>
+                                <Input
+                                    type="text"
+                                    required
+                                    name="firstname"
+                                    id="firstname"
+                                />
+                            </div>
+                            <div className="space-y-2">
+                                <Label
+                                    htmlFor="lastname"
+                                    className="block text-sm">
+                                    Lastname
+                                </Label>
+                                <Input
+                                    type="text"
+                                    required
+                                    name="lastname"
+                                    id="lastname"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="space-y-2">
+                            <Label
+                                htmlFor="email"
+                                className="block text-sm">
+                                Username
+                            </Label>
+                            <Input
+                                type="email"
+                                required
+                                name="email"
+                                id="email"
+                            />
+                        </div>
+
+                        <div className="space-y-0.5">
+                            <div className="flex items-center justify-between">
+                                <Label
+                                    htmlFor="pwd"
+                                    className="text-sm">
+                                    Password
+                                </Label>
+                                <Button
+                                    asChild
+                                    variant="link"
+                                    size="sm">
+                                    <Link
+                                        href="#"
+                                        className="link intent-info variant-ghost text-sm">
+                                        Forgot your Password ?
+                                    </Link>
+                                </Button>
+                            </div>
+                            <Input
+                                type="password"
+                                required
+                                name="pwd"
+                                id="pwd"
+                                className="input sz-md variant-mixed"
+                            />
+                        </div>
+
+                        <Button className="w-full">Sign In</Button>
+                    </div>
+
+                    <div className="my-6 grid grid-cols-[1fr_auto_1fr] items-center gap-3">
+                        <hr className="border-dashed" />
+                        <span className="text-muted-foreground text-xs">Or continue With</span>
+                        <hr className="border-dashed" />
+                    </div>
+
+                    <div className="grid grid-cols-2 gap-3">
                         <Button
                             type="button"
                             variant="outline">
@@ -68,73 +149,9 @@ export default function LoginPage() {
                             <span>Microsoft</span>
                         </Button>
                     </div>
-
-                    <hr className="my-4 border-dashed" />
-
-                    <div className="space-y-5">
-                        <div className="grid grid-cols-2 gap-3">
-                            <div className="space-y-2">
-                                <Label
-                                    htmlFor="firstname"
-                                    className="block text-sm">
-                                    Firstname
-                                </Label>
-                                <Input
-                                    type="text"
-                                    required
-                                    name="firstname"
-                                    id="firstname"
-                                />
-                            </div>
-                            <div className="space-y-2">
-                                <Label
-                                    htmlFor="lastname"
-                                    className="block text-sm">
-                                    Lastname
-                                </Label>
-                                <Input
-                                    type="text"
-                                    required
-                                    name="lastname"
-                                    id="lastname"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="space-y-2">
-                            <Label
-                                htmlFor="email"
-                                className="block text-sm">
-                                Username
-                            </Label>
-                            <Input
-                                type="email"
-                                required
-                                name="email"
-                                id="email"
-                            />
-                        </div>
-
-                        <div className="space-y-2">
-                            <Label
-                                htmlFor="pwd"
-                                className="text-sm">
-                                Password
-                            </Label>
-                            <Input
-                                type="password"
-                                required
-                                name="pwd"
-                                id="pwd"
-                                className="input sz-md variant-mixed"
-                            />
-                        </div>
-
-                        <Button className="w-full">Continue</Button>
-                    </div>
                 </div>
 
-                <div className="bg-muted rounded-(--radius) border p-3">
+                <div className="p-3">
                     <p className="text-accent-foreground text-center text-sm">
                         Have an account ?
                         <Button
