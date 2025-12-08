da2e3a5c4a7c62267dbe86a8ad6fe7cd2131dcb1
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-12-08 13:22:21 +0530

    feat: Implement Convex authentication middleware with defined public and bypass routes.

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -1,3 +1,4 @@
+export const isPublicRoutes = ['/auth(.*', '/']
 export const isBypassRoutes = [
     "/api/polar/webhook/*",
     "/api/auth(.*)",
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,18 @@
-import { convexAuthNextjsMiddleware, createRouteMatcher } from "@convex-dev/auth/nextjs/server";
-import { isBypassRoutes } from "./lib/permissions";
+import { convexAuthNextjsMiddleware, createRouteMatcher, nextjsMiddlewareRedirect } from "@convex-dev/auth/nextjs/server";
+import { isBypassRoutes, isPublicRoutes } from "./lib/permissions";
  
 
 const BypassMatcher = createRouteMatcher(isBypassRoutes);
+const PublicMatcher = createRouteMatcher(isPublicRoutes);
+
 export default convexAuthNextjsMiddleware(async (request, {convexAuth}) => {
   if(BypassMatcher(request)) {
     return ;
   }
   const authed = await convexAuth.isAuthenticated();
+  if(PublicMatcher(request)&& authed){
+    return nextjsMiddlewareRedirect(request, '/dashboard')
+  }
 });
  
 export const config = {
