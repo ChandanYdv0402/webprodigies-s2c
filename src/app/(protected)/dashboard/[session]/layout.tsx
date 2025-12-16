1327d29e535f4ce3c49f899c6bde1b24c25abb39
kundan223 <kundan0707y@gmail.com>
2025-12-16 17:11:43 +0530

    small changes

--- src/app/(protected)/dashboard/[session]/layout.tsx
+++ src/app/(protected)/dashboard/[session]/layout.tsx
@@ -1,8 +1,20 @@
+import { SubscriptionEntitlementQuery } from '@/convex/query.config'
+import { combinedSlug } from '@/lib/utils'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const layout = () => {
+type Props = {
+  children: React.ReacrNode
+}
+
+const layout = async ({children }: Props) => {
+  const { profileName , entitlement} = await SubscriptionEntitlementQuery()
+  if (!entitlement._valueJSON){
+    // TODO : remove the billing hardcoded path 
+    redirect(`/dashboard/${combinedSlug(profileName!)}`)
+  }
   return (
-    <div>layout</div>
+    <div>{children}</div>
   )
 }
 
--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,17 +1,14 @@
 import { SubscriptionEntitlementQuery } from '@/convex/query.config'
-import { redirect } from 'next/navigation'  // FIXED
+import { redirect } from 'next/navigation'
 
-//TODO: remove billing hardcoded  path 
-const page = async () => {   
+const page = async () => {
   const { entitlement, profileName } = await SubscriptionEntitlementQuery()
 
-  if (!entitlement._valueJSON) {
-    //redirect(`/billing/${combinedSlug(profileName!)}`)   
-    redirect(`/dashboard/${combinedSlug(profileName!)}`)   
+  if (!entitlement?._valueJSON) {
+    redirect(`/billing/${combinedSlug(profileName!)}`)
+  }
 
-  
-}
-redirect(`/dashboard/${combinedSlug(profileName!)}`)   
+  redirect(`/dashboard/${combinedSlug(profileName!)}`)
 }
 
 export default page
