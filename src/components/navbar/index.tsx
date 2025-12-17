6e0f9dc4a672f6314ab0d7e7121056cb0bc66ebd
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-12-17 18:42:46 +0530

    feat: implement Navbar component with dashboard navigation and project name display.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { useSearchParams } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 type Props = {}
@@ -10,6 +10,9 @@ const Navbar = (props: Props) => {
     const params = useSearchParams()
     const projectId = params.get('project')
     const profileName = params.get('profileName')
+    const pathname = usePathname()
+    const hasCanvas = pathname.includes ('canvas')
+    const hasStyleGuide = pathname.includes ('style-guide')
 
     return (
         <div className="grid grid-cols-2 lg:grid-cols-3 p-6 fixed top-0 left-0 right-0
@@ -22,13 +25,14 @@ const Navbar = (props: Props) => {
                 >
                     <div className="w-4 h-4 rounded-full bg-white"></div>
                 </Link>
-                {!hasCanvas ||}
+                {!hasCanvas ||
+                 (!hasStyleGuide && (
                 <div className="lg: inline-block hidden rounded-full text-primary/60 border
                             border-white/[0.12] backdrop-blur-xl bg-white/[0.08] px-4 py-2 text-sm
                             saturate-150">
                     Project / {project?.name}
                 </div>
-
+                ))}
             </div>
         </div>
     )
