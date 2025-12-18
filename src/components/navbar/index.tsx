023de00295e9c4d44057d4cccd2317348ea92c85
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-12-18 16:43:30 +0530

    feat: add Navbar component and configure Convex authentication with Google and Password providers.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,12 +6,41 @@ import React from 'react'
 import { useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
+import { CircleQuestionMark, Hash, User } from 'lucide-react'
+import { LayoutTemplate } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+import image from 'next/image'
 
-type Props = {}
-const Navbar = (props: Props) => {
+
+
+
+
+
+type TabsProps = {
+    label: string
+    href: string
+    icon?: React.ReactNode
+}
+
+const Navbar = () => {
     const params = useSearchParams()
     const projectId = params.get('project')
     const pathname = usePathname()
+
+
+    const tabs: TabsProps[] = [
+        {
+            label: 'Canvas',
+            href: `/dashboard//canvas?project=${projectId}`,
+            icon: <Hash className="h-4 w-4" />,
+        },
+        {
+            label: 'Style Guide',
+            href: `/dashboard//style-guide?project=${projectId}`,
+            icon: <LayoutTemplate className="h-4 w-4" />,
+        },
+    ]
     const project = useQuery(
         api.project.getProject,
         projectId ? { projectId: projectId as Id<'projects'> } : 'skip'
@@ -19,10 +48,9 @@ const Navbar = (props: Props) => {
     const hasCanvas = pathname.includes('canvas')
     const hasStyleGuide = pathname.includes('style-guide')
 
-
     return (
         <div className="grid grid-cols-2 lg:grid-cols-3 p-6 fixed top-0 left-0 right-0
-            Z-50">
+            z-50">
             <div className="flex items-center gap-4">
                 <Link
                     href={`/dashboard/`}
@@ -33,12 +61,50 @@ const Navbar = (props: Props) => {
                 </Link>
                 {!hasCanvas ||
                     (!hasStyleGuide && (
-                        <div className="lg: inline-block hidden rounded-full text-primary/60 border
+                        <div className="lg:inline-block hidden rounded-full text-primary/60 border
                             border-white/[0.12] backdrop-blur-xl bg-white/[0.08] px-4 py-2 text-sm
                             saturate-150">
-                            Project / {projectId}
+                            Project / {project?.name}
                         </div>
                     ))}
+            </div>
+            <div className="lg:flex hidden items-center justify-center gap-2">
+                <div className="flex items-center gap-2 backdrop-blur-xl bg-white/[0.08]
+                    border border-white/[0.12] rounded-full p-2 saturate-150">
+                    {tabs.map((t) => (
+                        <Link
+                            key={t.label}
+                            href={t.href}
+                            className={['group inline-flex items-center gap-2 rounded-full px-4 py-2 text-sm transition', `${pathname}?project=${projectId}` === t.href ? 'bg-white/[0.12] text-white border border-white/[0.16] backdrop-blur-sm'
+                                : 'text-zinc-400 hover:text-zinc-200 hover:bg-white/[0.06] border-white/[0.16] hover:border-transparent'
+                            ].join(' ')}
+                        >
+                            <span className={
+                                `${pathname}?project=${projectId}` === t.href ? 'opacity-100' : 'opacity-70 group-hover:opacity-90'
+                            }>
+                                {t.icon}
+                            </span>
+                            <span>{t.label}</span>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            <div className="flex items-center gap-4 justify-end">
+                <span className="text-sm text-white/50">TODO: credits</span>
+                <Button
+                variant="secondary"
+                className="rounded-full h-12 w-12 flex items-center justify-center
+                backdrop-blur-xl bg-white/[0.08] border border-white/[0.12] saturate-150
+                hover:bg-white/[0.12]"
+                >
+                    <CircleQuestionMark className = "size-5 text-white"/>
+                    </Button>
+                    <Avatar className="size-12 ml-2">
+                        <AvatarImage  />
+                        <AvatarFallback>
+                        <User className="size-5 text-black" />
+                        </AvatarFallback>
+                    </Avatar>
             </div>
         </div>
     )
