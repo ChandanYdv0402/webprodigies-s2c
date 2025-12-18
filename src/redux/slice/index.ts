6722870fd78f8377b2a56ecc679cfa0af2bcaaf9
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-12-19 02:43:14 +0530

    feat: Introduce project creation functionality and a dedicated viewport Redux slice.

--- src/redux/slice/index.ts
+++ src/redux/slice/index.ts
@@ -1,8 +1,12 @@
-import { Reducer } from '@reduxjs/toolkit';
-import projectsReducer from './projects';
-import profileReducer from './profile';
+import { Reducer } from '@reduxjs/toolkit'
+import profile from './profile'
+import projects from './projects'
+import shapes from './shaped'
+import viewport from './viewport'
 
 export const slices: Record<string, Reducer> = {
-    projects: projectsReducer,
-    profile: profileReducer,
-};
+  profile,
+  projects,
+  shapes,
+  viewport,
+}
