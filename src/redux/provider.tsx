5d166734a6e0c6919fda15508014714dac191c4f
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-12-19 00:11:05 +0530

    feat: add Redux provider component

---
+++ src/redux/provider.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React, { ReactNode, useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from './store'
+import { RootState } from './store'
+
+const ReduxProvider = ({
+    children,
+    preloadedState,
+}: {
+    children: ReactNode
+    preloadedState?: Partial<RootState>
+}) => {
+    const storeRef = useRef<AppStore>()
+    if (!storeRef.current) {
+        storeRef.current = makeStore(preloadedState)
+    }
+
+    return <Provider store={ storeRef.current }> { children } </Provider>
+}
+
+export default ReduxProvider
