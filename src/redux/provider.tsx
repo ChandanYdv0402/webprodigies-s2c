dc099a01219b18e78593a0ab53e23ef882392b52
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-12-19 07:29:58 +0530

    feat: implement ReduxProvider component for store initialization and auth synchronization

---
+++ src/redux/provider.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React, { ReactNode, useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from './store'
+import { RootState } from './store'
+import { ReduxAuthSync } from '@/components/providers/ReduxAuthSync'
+
+const ReduxProvider = ({
+    children,
+    preloadedState,
+}: {
+    children: ReactNode
+    preloadedState?: Partial<RootState>
+}) => {
+    const storeRef = useRef<AppStore>()
+    if (!storeRef.current) {
+        storeRef.current = makeStore(preloadedState)
+    }
+
+    return <Provider store={storeRef.current}>
+        <ReduxAuthSync />
+        {children}
+    </Provider>
+}
+
+export default ReduxProvider
