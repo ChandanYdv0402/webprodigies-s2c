4afc2110502f1b6d923b828f3f88aff71c4174e4
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-12-19 05:45:34 +0530

    feat: add style guide page with color and typography data structures.

--- src/app/(protected)/dashboard/[session]/(workspace)/style-guide/page.tsx
+++ src/app/(protected)/dashboard/[session]/(workspace)/style-guide/page.tsx
@@ -1,29 +1,46 @@
-import { StyleGuideQuery } from '@/lib/convex-queries'
-import { TabsContent } from '@components/ui/tabs'
+import { StyleGuideQuery } from '@/convex/query.config'
+import { TabsContent } from '@/components/ui/tabs'
 import React from 'react'
+import { StyleGuide } from '@redux/api/style-guide'
 
 type Props = {
-  searchParams: Promise<{
-    project: string
-  }>
+    searchParams: Promise<{
+        project: string
+    }>
 }
 
 const Page = async ({ searchParams }: Props) => {
-  const projectId = (await searchParams).project
-  const existingStyleGuide = await StyleGuideQuery(projectId)
+    const projectId = (await searchParams).project
 
-  if (!existingStyleGuide) {
+    if (!projectId || projectId === 'null') {
+        return <div>Project not found</div>
+    }
+
+    const existingStyleGuide = await StyleGuideQuery(projectId)
+
+    const guide =
+        existingStyleGuide.styleGuide?._valueJSON as StyleGuide
+
+
+    const colorguide = existingStyleGuide.styleGuide?._valueJSON as unknown as StyleGuide
+
+    const colorGuide = colorguide?.colorSections || []
+    const typographyGuide = colorguide?.typographySections || []
+
+    
+
+
     return (
-      <div>
-        <TabsContent
-          value="colours"
-          className="space-y-8"
-        >
-          
-        </TabsContent>
-      </div>
+        <div>
+            <TabsContent
+                value="colours"
+                className="space-y-8"
+            >
+
+            </TabsContent>
+        </div>
     )
-  }
 }
+
 
 export default Page
